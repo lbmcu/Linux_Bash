5fd872dfe04708880917872bb0894d823ed06d04
锅盖小郭哥 <1373766226@qq.com>
2021-04-22 16:45:07 +0800

    Add isLoad bootloader/partition-table && Usage

--- esp32build.sh
+++ esp32build.sh
@@ -4,6 +4,13 @@
 COM_PORT=$1
 #bin文件
 file=$2
+#是否烧录bootloader/partition-table
+if [ !$3 ]
+then
+	isboot=$3
+else
+	isboot=0
+fi
 #工程目录
 prjDir="/mnt/c/Users/ABC/Desktop/ESP32C3_MINI1/Code/esp-aws-iot/examples/subscribe_publish"
 #烧录文件夹
@@ -11,16 +18,22 @@ desDir="/mnt/c/Users/ABC/Desktop/ESP32C3_MINI1/out"
 #Windows下烧录文件夹
 winDir="C:/Users/ABC/Desktop/ESP32C3_MINI1/out"
 
-#烧录参数
-buildFlag="-b 921600 --before default_reset --after hard_reset --chip esp32c3  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x10000"
-
 #判断参数
-if [ $# != 2 ]
+if [ $# != 3 ]
 then
-	echo "Usage: ./esp32build.sh [COMx] [Prj_Name]"
+	echo -e "Usage: ./esp32build.sh [COMx] [Prj_Name] [isboot]\n"
 	exit
 else
-	echo "----------------------------------------"
+	echo -e "\n###############################################################\n"
+fi
+
+#烧录参数
+#0x0 $winDir/bootloader.bin 0x8000 $winDir/partition-table.bin
+if [ $isboot == 0 ]
+then
+	buildFlag="-b 921600 --before default_reset --after hard_reset --chip esp32c3  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x10000 "
+else
+	buildFlag="-b 921600 --before default_reset --after hard_reset --chip esp32c3  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x0 $winDir/bootloader.bin 0x8000 $winDir/partition-table.bin 0x10000 "
 fi
 
 #进入工程目录
@@ -48,6 +61,16 @@ else
 	rm -rf $file
 	cp build/$file $desDir
 fi
+
+#是否需要拷贝bootloader/partition-table
+if [ $isboot == 0 ]
+then
+	echo -e "------------- Not cp bootloader/partition-table -------------\n"
+else
+	cp build/bootloader/bootloader.bin $desDir
+	cp build/partition_table/partition-table.bin $desDir
+fi
+
 echo -e "------------- 2. Ending to make Dect Bin file --------------\n"
 
 # 执行Python烧录
